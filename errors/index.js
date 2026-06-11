75873c0662af214d9529ca68f8f5b37ff3c32bd9
DevgenX <sebgonzales.dev@gmail.com>
2026-06-11 17:13:04 +0800

    chore: remove legacy server code

--- errors/index.js
+++
@@ -1,5 +0,0 @@
-import BadRequestError from "./bad-request.js";
-import NotFoundError from "./not-found.js";
-import unAuthenticatedError from "./unauthenticated.js";
-
-export { BadRequestError, NotFoundError, unAuthenticatedError };
